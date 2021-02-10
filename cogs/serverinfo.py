c7646f0cebad4e58909b40c2f553a8979f09a59a
Andre Saddler <dresadd09691@gmail.com>
2021-02-09 22:40:01 -0500

    cogs: update serverinfo

--- cogs/serverinfo.py
+++ cogs/serverinfo.py
@@ -1,9 +1,15 @@
+import iso8601
 import discord
 import requests
+import os
 from datetime import datetime
 from urllib.parse import unquote
 from discord.ext import commands
 from discord.ext.commands.cooldowns import BucketType
+from dotenv import load_dotenv
+
+load_dotenv()
+BMSID = os.getenv('battleMetricsServerID')
 
 
 def BattleMetricsAPI():
@@ -46,54 +52,63 @@ class ServerInfo(commands.Cog, name='Rust'):
 
         await ctx.send(file=file, embed=embed)
 
-    # #  RustyCorns server info
-    # @commands.command(name='RustyCorns Server info',
-    #                   aliases=['rcstatus'],
-    #                   help='RustyCorns server status')
-    # @commands.cooldown(2, 60, BucketType.user)
-    # async def server_status(self, ctx):
+    #  RustyCorns server info
+    @commands.command(name='RustyCorns Status',
+                      aliases=['rcstatus'],
+                      help='RustyCorns server status')
+    @commands.cooldown(2, 60, BucketType.user)
+    async def server_status(self, ctx):
 
-    #     header = ""
-    #     title = ""
-    #     url = ""
-    #     responseJSON = BattleMetricsAPI()
+        header = ""
+        title = ""
+        url = ""
+        responseJSON = BattleMetricsAPI()
 
-    #     # JSON Results Mapping
-    #     header = responseJSON['data']['attributes']['details']['rust_headerimage']
-    #     title = responseJSON['data']['attributes']['name']
-    #     desc = responseJSON['data']['attributes']['details']['rust_description']
-    #     url = responseJSON['data']['attributes']['details']['rust_url']
-    #     fps = responseJSON['data']['attributes']['details']['rust_fps']
-    #     fpsavg = responseJSON['data']['attributes']['details']['rust_fps_avg']
-    #     rust_type = responseJSON['data']['attributes']['details']['rust_type']
-    #     rust_uptime = responseJSON['data']['attributes']['details']['rust_uptime']
+        # JSON Results Mapping
+        header = responseJSON['data']['attributes']['details']['rust_maps']['url']
+        embed_image = responseJSON['data']['attributes']['details']['rust_maps']['thumbnailUrl']
+        maps = responseJSON['data']['attributes']['details']['map']
 
-    #     embed = discord.Embed(
-    #         title=title,
-    #         description='***' + desc + '***' + '\n\n' + url,
-    #         # crimson color code
-    #         colour=(0xDC143C)
-    #     )
-    #     embed.set_image(url=header)
-    #     file = discord.File("./assets/images/rust_honeycomb.png", filename="rust_honeycomb.png")
-    #     embed.set_thumbnail(url="attachment://rust_honeycomb.png")
-    #     # embed.add_field(name="Total Time Played", value=(TTP), inline=True)
-    #     # embed.add_field(name=('Kills'), value=(kills), inline=True)
-    #     # embed.add_field(name=("Deaths"), value=(deaths), inline=True)
-    #     # embed.add_field(name="Wins", value=(wins), inline=True)
-    #     # embed.add_field(name="Win %", value=(winr), inline=True)
-    #     # embed.add_field(name="KDR", value=(kdr), inline=True)
-    #     # embed.add_field(name="Assists", value=(asst), inline=True)
-    #     # embed.add_field(name="Matches", value=(matches), inline=True)
-    #     # embed.add_field(name="Headshots", value=(headshots), inline=True)
-    #     # embed.add_field(name="Headshots %", value=(headshotpercentage), inline=True)
-    #     # embed.add_field(name="First Bloods", value=(firstbloods), inline=True)
-    #     # embed.add_field(name="Aces", value=(aces), inline=True)
-    #     # embed.add_field(name="Clutches", value=(clutches), inline=True)
-    #     # embed.add_field(name="Flawless", value=(flawless), inline=True)
-    #     # embed.add_field(name=" .", value=(". "), inline=True)
+        title = responseJSON['data']['attributes']['name']
+        desc = responseJSON['data']['attributes']['details']['rust_description']
+        url = responseJSON['data']['attributes']['details']['rust_url']
+        fps = responseJSON['data']['attributes']['details']['rust_fps']
+        fpsavg = responseJSON['data']['attributes']['details']['rust_fps_avg']
+        rust_type = responseJSON['data']['attributes']['details']['rust_type']
+        rust_uptime = responseJSON['data']['attributes']['details']['rust_uptime']
+        rust_status = responseJSON['data']['attributes']['status']
+        rust_world_size = responseJSON['data']['attributes']['details']['rust_world_size']
+        rust_world_seed = responseJSON['data']['attributes']['details']['rust_world_seed']
+        # players = responseJSON['data']['attributes']['details']['players']
+        last_wipe = responseJSON['data']['attributes']['details']['rust_last_wipe']
 
-    #     await ctx.send(file=file, embed=embed)
+        #convert iso8601 format
+        is8061 = ""
+        is8061 = iso8601.parse_date(last_wipe)
+
+        # get country flag iso code
+        country = responseJSON['data']['attributes']['country']
+        if country == 'US':
+            flag = (u"\U0001F1FA\U0001F1F8" + " " + country)
+
+        embed = discord.Embed(
+            title=title,
+            description='\n\n' + header + '\n\n',
+            # crimson color code
+            colour=(0xDC143C)
+        )
+        embed.set_image(url=embed_image)
+        file = discord.File("./assets/images/rust_honeycomb.png", filename="rust_honeycomb.png")
+        embed.set_thumbnail(url="attachment://rust_honeycomb.png")
+        embed.add_field(name="Status", value=(rust_status), inline=True)
+        embed.add_field(name=("Type"), value=(rust_type), inline=True)
+        embed.add_field(name=('FPS'), value=(fpsavg), inline=True)
+        embed.add_field(name=('Rust World Size'), value=(rust_world_size), inline=True)
+        embed.add_field(name=('Rust World Seed'), value=(rust_world_seed), inline=True)
+        embed.add_field(name=('Last Wipe'), value=(is8061), inline=True)
+        embed.add_field(name=('Country'), value=(flag), inline=True)
+
+        await ctx.send(file=file, embed=embed)
 
 
 def setup(bot):
